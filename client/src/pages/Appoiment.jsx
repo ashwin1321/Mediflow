7dff1d7e44d293d778af84541f3358e47be86020
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-02 08:27:25 +0545

    page format created

---
+++ client/src/pages/Appoiment.jsx
@@ -0,0 +1,14 @@
+import Layout from '../pages/Layout'
+import React from 'react'
+
+const Appoiment = () => {
+    return (
+        <Layout>
+            <div style={{ margin: "5%" }} >
+                <h1 >Appoiment</h1>
+            </div>
+        </Layout>
+    )
+}
+
+export default Appoiment
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import Layout from '../pages/Layout'
+import React from 'react'
+
+const Login = () => {
+    return (
+        <Layout>
+            <h1>Login</h1>
+        </Layout>
+    )
+}
+
+export default Login
