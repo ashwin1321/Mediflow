5c77d23cab986a856c5575598adab0075d0e6406
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-24 22:38:05 +0545

    blog page updated

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -1,95 +1,153 @@
 import Layout from "../pages/Layout";
 import React from "react";
 import "../styles/Blogs.css";
-import { blog1, blog2, blog3 } from "../assets/Homepage";
+import { health1, health2, health3, tech1, tech2, wel1, wel2 } from "./imgg";
 
 const Blogs = () => {
   return (
     <Layout>
-      <section class="blogs" id="blogs">
-        <h1 class="headinsg">
-          our <span>blogs</span>
-        </h1>
-
-        <div class="box-container">
-          <div class="box">
-            <div class="image">
-              <img src={blog1} alt="" />
-            </div>
-            <div class="content">
-              <div class="icon">
-                <a href="#">
-                  {" "}
-                  <i class="fas fa-calendar"></i> 1st may, 2021{" "}
-                </a>
-                <a href="#">
-                  {" "}
-                  <i class="fas fa-user"></i> by admin{" "}
-                </a>
-              </div>
-              <h3>blog title goes here</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Provident, eius.
-              </p>
-              <a href="#" class="btn">
-                {" "}
-                learn more <span class="fas fa-chevron-right"></span>{" "}
-              </a>
+      <section id="projects" className="py-5">
+        <div className="container">
+          {/* <!--     title--> */}
+          <div className="row title mb-3">
+            <div className="col d-flex justify-content-center text-center mb-5">
+              <h1 className="text-uppercase">
+                OUR <span className="primary-color">BLOGS</span>
+              </h1>
             </div>
           </div>
-
-          <div class="box">
-            <div class="image">
-              <img src={blog2} alt="" />
-            </div>
-            <div class="content">
-              <div class="icon">
-                <a href="#">
-                  {" "}
-                  <i class="fas fa-calendar"></i> 1st may, 2021{" "}
-                </a>
-                <a href="#">
-                  {" "}
-                  <i class="fas fa-user"></i> by admin{" "}
-                </a>
+          {/* <!--      end of title-->
+      <!--      button row--> */}
+          <div className="row">
+            <div className="col text-center">
+              <div
+                className="btn-group btn-group-lg mb-5 button-group filter-button-group"
+                role="group"
+              >
+                <h1 className="text-uppercase mt-4">
+                  OUR <span className="primary-color">BLOGS</span>
+                </h1>
               </div>
-              <h3>blog title goes here</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Provident, eius.
-              </p>
-              <a href="#" class="btn">
-                {" "}
-                learn more <span class="fas fa-chevron-right"></span>{" "}
-              </a>
             </div>
           </div>
-
-          <div class="box">
-            <div class="image">
-              <img src={blog3} alt="" />
-            </div>
-            <div class="content">
-              <div class="icon">
-                <a href="#">
-                  {" "}
-                  <i class="fas fa-calendar"></i> 1st may, 2021{" "}
-                </a>
-                <a href="#">
-                  {" "}
-                  <i class="fas fa-user"></i> by admin{" "}
-                </a>
+          {/* <!--      images row--> */}
+          <div className="row d-flex grid">
+            <div className="col-sm-6 col-md-4 my-3 wellness">
+              <div className="card">
+                <img className="card-img-top" src={health1} alt="Card" />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
               </div>
-              <h3>blog title goes here</h3>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Provident, eius.
-              </p>
-              <a href="#" class="btn">
-                {" "}
-                learn more <span class="fas fa-chevron-right"></span>{" "}
-              </a>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 healthcare">
+              <div className="card">
+                <img className="card-img-top" src={health2} alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 medicaltech">
+              <div className="card">
+                <img className="card-img-top" src={health3} alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 medicaltech">
+              <div className="card">
+                <img className="card-img-top" src={tech1} alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 wellness">
+              <div className="card">
+                <img className="card-img-top" src={tech2} alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 healthcare">
+              <div className="card">
+                <img className="card-img-top" src={wel1} alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 wellness">
+              <div className="card">
+                <img className="card-img-top" src={wel2} alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 healthcare">
+              <div className="card">
+                <img className="card-img-top" src={health1} s alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 my-3 medicaltech">
+              <div className="card">
+                <img className="card-img-top" src={tech2} alt="Card " />
+                <div className="card-body">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
---
+++ client/src/pages/imgg/index.js
@@ -0,0 +1,10 @@
+import health1 from "./img/health-1.jpg";
+import health2 from "./img/health-2.jpg";
+import health3 from "./img/health-3.jpg";
+import tech1 from "./img/technology-1.jpg";
+import tech2 from "./img/technology-2.jpg";
+import wel1 from "./img/wellness-1.jpg";
+import wel2 from "./img/wellness-2.jpg";
+import wel3 from "./img/wellness-3.jpg";
+
+export { health1, health2, health3, tech1, tech2, wel1, wel2, wel3 };
