e32ff0caa77fdd5b27655339366a34f1f9375702
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-25 12:37:30 +0545

    table created

--- server/tables/reports.js
+++ server/tables/reports.js
@@ -3,12 +3,14 @@ const { connectDb, client } = require("../models/db");
 const createReportTable = async () => {
   try {
     await client.query(
-      "CREATE TABLE IF NOT EXISTS reports (" +
+      "CREATE TABLE IF NOT EXISTS report (" +
         "rid varchar(100) PRIMARY KEY," +
         "name VARCHAR(255) NOT NULL," +
         "size INTEGER NOT NULL," +
         "mimetype VARCHAR(255) NOT NULL," +
-        "data BYTEA" +
+        "data BYTEA," +
+        "pid varchar(100) NOT NULL," +
+        "FOREIGN KEY (pid) REFERENCES patients(pid) ON DELETE CASCADE" +
         ")"
     );
   } catch (error) {
