a2de291f43eae2c6af3089c9cbf4fd75ce9a7d60
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-28 22:17:37 +0545

    login reigster fields state set

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,32 @@
 import Layout from "../pages/Layout";
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Login.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [role, setRole] = useState("patient");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const data = { role, email, password };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
   return (
     <Layout>
       <div className="form-container mt-5">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2>Sign In</h2>
           <div className="form-group">
             <label htmlFor="role">Role</label>
-            <select name="role" id="role">
+            <select
+              name="role"
+              id="role"
+              onChange={(e) => setRole(e.target.value)}
+            >
               <option value="patient">Patient</option>
               <option value="doctor">Doctor</option>
               <option value="lab">Lab</option>
@@ -20,12 +35,22 @@ const Login = () => {
           </div>
           <div className="form-group">
             <label htmlFor="email">Email address:</label>
-            <input type="email" id="email" placeholder="Enter email" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" placeholder="Enter password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
 
           <button type="submit">Sign in</button>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,8 +3,18 @@ import React, { useState } from "react";
 import "../styles/Register.css";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [Citizenship, setCitizenship] = useState("");
+  const [password, setPassword] = useState("");
+  const pid = "P" + Math.floor(Math.random() * 100000);
+
+  const data = { name, age, email, Citizenship, password, pid };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(data);
   };
   return (
     <Layout>
@@ -13,11 +23,21 @@ const Register = () => {
           <h2>Sign Up</h2>
           <div className="form-group">
             <label htmlFor="email">Name:</label>
-            <input type="text" id="name" placeholder="Enter Your Name" />
+            <input
+              type="text"
+              id="name"
+              placeholder="Enter Your Name"
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="email">Age:</label>
-            <input type="text" id="age" placeholder="Enter Your Age" />
+            <input
+              type="text"
+              id="age"
+              placeholder="Enter Your Age"
+              onChange={(e) => setAge(e.target.value)}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="email">Citizenship Number:</label>
@@ -25,22 +45,25 @@ const Register = () => {
               type="text"
               id="number"
               placeholder="Enter Your Citizenship Number"
+              onChange={(e) => setCitizenship(e.target.value)}
             />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email address:</label>
-            <input type="email" id="email" placeholder="Enter email" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" placeholder="Enter password" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="confirm-password">Confirm Password:</label>
             <input
               type="password"
-              id="confirm-password"
-              placeholder="Confirm password"
+              id="password"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
