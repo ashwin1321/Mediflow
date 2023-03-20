fd7b80b42fb301e2b343cf6eec2e353fcc2b8c18
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-03-20 10:04:17 +0545

    navbar created

--- client/src/pages/admin/manageDoctor.jsx
+++ client/src/pages/admin/manageDoctor.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Dasboard from '../Dasboard'
@@ -14,6 +15,9 @@ const manageDoctor = () => {
     const [role, setRole] = useState("doctor")
 
     const data = { role, doctorName, did }
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isAddDoctor, setIsAddDoctor] = useState(false)
 
     const handleDeleteDoctor = async () => {
         await axios.delete(`http://localhost:5000/get/doc`, did)
@@ -36,8 +40,8 @@ const manageDoctor = () => {
 
     }
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
+    const handleSearchDoctor = async () => {
+        console.log(doctorName)
         axios.get('http://localhost:5000/get/doc', {
             params: {
                 name: doctorName
@@ -49,11 +53,19 @@ const manageDoctor = () => {
             .catch(err => {
                 console.log(err)
             })
+    }
 
-    }, [doctorName])
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        handleSearchDoctor()
+
+    }, [])
 
 
     return (
+
+
         <div className="dashboard-container" >
 
             <div>
@@ -61,10 +73,45 @@ const manageDoctor = () => {
             </div>
 
             <div className="main-content">
+                {/* search and add doctor */}
+                <div>
+                    <div className=" shadow-sm">
+                        {/* Header */}
+                        <div className="   ">
+                            <div className='flex flex-row gap-3 justify-between items-center '>
+                                <div className='mt-5'>
+                                    <input type="text" placeholder="Search Doctor" className="border-2 border-gray-300 bg-white   rounded-lg text-xl focus:outline-none"
+                                        onChange={(e) => setDoctor(e.target.value)}
+                                        onClick={handleSearchDoctor}
+                                    />
+                                </div>
 
-                admin
-            </div>
+                                <div className=''>
+                                    <button className="bg-green-900 hover:bg-green-700 text-white font-bold py-3 px-4 rounded-3 right-0"
+                                        onClick={() => setIsAddDoctor(!isAddDoctor)}
+                                    >Add Doctor</button>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+                {/* add doctor modal */}
+
+                {/*  show data in table */}
+
+
+
+
+            </div >
         </div >
+
+
     )
 }
 
