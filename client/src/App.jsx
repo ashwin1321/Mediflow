670b83da844589b6f28aa728b2f08a8ef85858c7
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-16 21:34:07 +0545

    feat: protected routes for the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeMain from "./pages/HomeMain";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -15,6 +16,7 @@ import Diagnosis from "./pages/doc/Diagonosis";
 import ViewReport from "./pages/viewReport";
 import Report from "./pages/patient/reports";
 import ViewReports from "./pages/lab/ViewReports";
+import Cookies from "js-cookie";
 
 
 function App() {
@@ -24,20 +26,29 @@ function App() {
 
                 <Route path="/" element={<HomeMain />} />
                 <Route path="/blogs" element={<Blogs />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/book" element={<Appoiment />} />
+
+                <Route path="/login" element={<IsLogIn><Login /></IsLogIn>} />
+                <Route path="/register" element={<IsLogIn><Register /></IsLogIn>} />
+
+                <Route path="/book" element={<IsLoggedIn><IsPatient><Appoiment /></IsPatient></IsLoggedIn>} />
+
                 <Route path="/otp" element={<Otp />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-                <Route path='/dashboard/detail' element={<Detail />} />
-                <Route path='/dashboard/manageDoc' element={<ManageDoctor />} />
-                <Route path='/dashboard/manageLab' element={<ManageLab />} />
+                <Route path='/dashboard/detail' element={<IsPatient><Detail /></IsPatient>} />
+
+                <Route path='/dashboard/manageDoc' element={<IsAdmin><ManageDoctor /> </IsAdmin>} />
+                <Route path='/dashboard/manageLab' element={<IsAdmin><ManageLab /></IsAdmin>} />
+
                 <Route path='/dashboard/viewAppointment' element={<ViewAppointment />} />
+
                 <Route path="*" element={<h1>404 Not Found</h1>} />
-                <Route path="/dashboard/diagnosis" element={< Diagnosis />} />
-                <Route path="/dashboard/report" element={< ViewReport />} />
-                <Route path="/dashboard/report/patient" element={< Report />} />
-                <Route path="/dashboard/report/lab" element={< ViewReports />} />
+
+                <Route path="/dashboard/diagnosis" element={<IsDoctor>< Diagnosis /></IsDoctor>} />
+                <Route path="/dashboard/report" element={<IsDoctor>< ViewReport /></IsDoctor>} />
+
+                <Route path="/dashboard/report/patient" element={<IsPatient>< Report /></IsPatient>} />
+
+                <Route path="/dashboard/report/lab" element={<IsLab>< ViewReports /></IsLab>} />
 
 
             </Routes>
@@ -46,3 +57,130 @@ function App() {
 }
 
 export default App;
+
+export function IsAdmin(props) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = Cookies.get("token");
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate]);
+
+    const role = Cookies.get("role");
+    if (role === "admin") {
+        return props.children;
+    } else {
+        return (
+            <div className="text-center text-3xl mt-[20%] text-red-800">
+                <h1>Not Authorized!</h1>
+                <h1>Please go back!</h1>
+            </div>
+        );
+    }
+}
+
+export function IsDoctor(props) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = Cookies.get("token");
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate]);
+
+    const role = Cookies.get("role");
+    if (role === "doctor") {
+        return props.children;
+    } else {
+        return (
+            <div className="text-center text-3xl mt-[20%] text-red-800">
+                <h1>Not Authorized!</h1>
+                <h1>Please go back!</h1>
+            </div>
+        );
+    }
+}
+
+export function IsPatient(props) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = Cookies.get("token");
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate]);
+
+    const role = Cookies.get("role");
+    if (role === "patient") {
+        return props.children;
+    } else {
+        return (
+            <div className="text-center text-3xl mt-[20%] text-red-800">
+                <h1>Not Authorized!</h1>
+                <h1>Please go back!</h1>
+            </div>
+        );
+    }
+}
+
+export function IsLab(props) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = Cookies.get("token");
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate]);
+
+    const role = Cookies.get("role");
+    if (role === "lab") {
+        return props.children;
+    } else {
+        return (
+            <div className="text-center text-3xl mt-[20%] text-red-800">
+                <h1>Not Authorized!</h1>
+                <h1>Please go back!</h1>
+            </div>
+        );
+    }
+}
+
+export function IsLoggedIn({ children }) {
+    const navigate = useNavigate();
+    const token = Cookies.get("token");
+
+    useEffect(() => {
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate, token]);
+
+    if (token) {
+        console.log("here");
+        return children;
+    }
+
+    return null;
+}
+export function IsLogIn({ children }) {
+    const navigate = useNavigate();
+    const token = Cookies.get("token");
+
+    useEffect(() => {
+        if (token) {
+            navigate("/dashboard");
+        }
+    }, [navigate, token]);
+
+    if (!token) {
+        console.log("here");
+        return children;
+    }
+
+    return null;
+}
